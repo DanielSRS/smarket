bd051a72816369e4be3e1c294a50e1e7d6ec3d66
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2023-08-20 16:30:11 -0300

    🚧 Move createAndBindSocket para o arquivo socket

--- socket.c
+++ socket.c
@@ -1,6 +1,7 @@
 #include <netdb.h> //addrinfo
 #include <stdio.h> // null e stderr
 #include <string.h> // memset
+#include <unistd.h> // cloes
 
 #define PORT "3492"  // A porta usada para outros usuários se conectarem. 
 
@@ -32,4 +33,73 @@ struct addrinfo* getAddressInfo(void (*onError)()) {
     }
 
     return addressInfo;
+}
+
+/**
+ * Cria um socket e faz o bind com a porta
+*/
+int createAndBindSocket(const struct addrinfo *serverAddressInfo, void (*onError)()) {
+  const struct addrinfo *addressItem;
+  int INVALID_FILE_DESCRIPTOR = -1, ERROR_ON_SET_SOCKET_OPTION = -1; // Ocoreu um ero na criação do socket
+  int yes = 1;
+  int socketFileDescriptor = -1;
+
+  // Para cada item da lista de endereços, tenta criar e fazer bind.
+  // para quando for concluido com sucesso em algum item da lista
+  // Se houver erro, tentar com o proximo item até a lista acabar
+  for(addressItem = serverAddressInfo; addressItem != NULL; addressItem = addressItem->ai_next) {
+
+    // Cria o socket
+    socketFileDescriptor = socket(
+      addressItem->ai_family,
+      addressItem->ai_socktype,
+      addressItem->ai_protocol
+    );
+
+    if (socketFileDescriptor == INVALID_FILE_DESCRIPTOR) {
+      perror("server: socket");
+      continue;                   // Tenta cria o socket com o próximo item da lista
+    }
+
+    // Força o reuso do endereço pra evitar erros de porta ocupada
+    int socketOptionRes = setsockopt(
+      socketFileDescriptor,
+      SOL_SOCKET,
+      SO_REUSEADDR,
+      &yes,
+      sizeof(int)
+    );
+
+    if (socketOptionRes == ERROR_ON_SET_SOCKET_OPTION) {
+        perror("setsockopt");
+        onError();
+    }
+
+    // Faz o bind com a porta 
+    int bindSocketResponse = bind(
+      socketFileDescriptor,
+      addressItem->ai_addr,
+      addressItem->ai_addrlen
+    );
+
+    /** Se não for possivel fazer o bind*/
+    if (bindSocketResponse == -1) {
+        close(socketFileDescriptor);
+        perror("server: bind");
+        continue;
+    }
+
+    break; // Não houve erro. Encerra iteração
+  }
+
+  /**
+   * Iterou a lista inteira e não foi possivel criar socket e fazer bind com 
+   * nenum do itens
+  */
+  if (addressItem == NULL)  {
+    fprintf(stderr, "server: failed to bind\n");
+    onError();
+  }
+
+  return socketFileDescriptor;
 }
--- socket.h
+++ socket.h
@@ -2,5 +2,6 @@
 #define	SOCKET_H
 
 struct addrinfo* getAddressInfo(void (*onError)());
+int createAndBindSocket(const struct addrinfo *serverAddressInfo, void (*onError)());
 
 #endif
