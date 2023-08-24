31c0f67ffe1f858c14ded010a6430c307929c40e
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2023-08-24 01:09:40 -0300

    ✅ Verifica se sibling é nulo em novo MapEntry

--- tests/MapEntryTest.c
+++ tests/MapEntryTest.c
@@ -10,6 +10,15 @@ TestResult _newMapEntryCreatesANonNullReference() {
   return expectToBeNotNull(entry);
 }
 
+TestResult _newMapEntrySiblingAttrShouldBeNull() {
+  char *key = "idDaEntrada";
+  char *value = "Sou um valor qualquer";
+  char *type = "Custom type";
+  MapEntry *entry = newMapEntry(key, (void *) value, type);
+
+  return expectToBeNull(entry->sibling);
+}
+
 
 int main(int argc, char **argv){
   TestArgs args = parseTestArgs(argc, argv);
@@ -17,6 +26,9 @@ int main(int argc, char **argv){
   SWITCH (args.testName) {
     CASE ("_newMapEntryCreatesANonNullReference")
       it("Retorna uma referência válida ao criar novo MapEntry", _newMapEntryCreatesANonNullReference);
+      break;
+    CASE ("_newMapEntrySiblingAttrShouldBeNull")
+      it("Atributo sibling de novo MapEntry deve ser NULL", _newMapEntrySiblingAttrShouldBeNull);
       break;
     DEFAULT
       noTestFoundWithGiven(args.testName);
