0fe2aec647b517854dcda117b024e5343b40a1a9
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2023-08-30 13:44:39 -0300

    ✨ Adiciona biblioteca ReadRFIDTags usando a MercuryApi

---
+++ src/libs/MercuryApi/ReadRFIDTags.h
@@ -0,0 +1,11 @@
+#include "../../util/Map/Map.h"
+
+#ifndef READRFIDTAGS_H
+#define READRFIDTAGS_H
+
+/**
+ * Le as tags e salva os ids no buffer
+ */
+int readRFIDTags(int argc, char **argv,  Map * result);
+
+#endif //READTAGS_H
