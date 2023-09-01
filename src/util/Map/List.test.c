be161f3fce3636aa8e75ece79b925f565e372a3f
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2023-09-01 12:41:38 -0300

    ✅ Atributo _map de uma nova Lista não pode ser NULL

--- src/util/Map/List.test.c
+++ src/util/Map/List.test.c
@@ -7,12 +7,21 @@ TestResult newListCreatesANonNullReference() {
   return expectToBeNotNull(list);
 }
 
+TestResult _mapAttrShouldBeNotNull() {
+  List *list = newList();
+
+  return expectToBeNotNull(list->_map);
+}
+
 int main(int argc, char **argv){
   TestArgs args = parseTestArgs(argc, argv);
 
   SWITCH (args.testName) {
     CASE ("newListCreatesANonNullReference")
       it("Retorna uma referência válida ao criar nova List", newListCreatesANonNullReference);
+      break;
+    CASE ("_mapAttrShouldBeNotNull")
+      it("Atributo _map de uma nova Lista não pode ser NULL", _mapAttrShouldBeNotNull);
       break;
     DEFAULT
       noTestFoundWithGiven(args.testName);
