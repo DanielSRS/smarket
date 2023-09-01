2ba9348eb60040434528fb228b6190567270460a
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2023-09-01 12:45:25 -0300

    ✅ Atributo destroy tem valor válido (não nulo)

--- src/util/Map/List.test.c
+++ src/util/Map/List.test.c
@@ -13,6 +13,12 @@ TestResult _mapAttrShouldBeNotNull() {
   return expectToBeNotNull(list->_map);
 }
 
+TestResult destroyShouldBeNotNull() {
+  List *list = newList();
+
+  return expectToBeNotNull(list->destroy);
+}
+
 int main(int argc, char **argv){
   TestArgs args = parseTestArgs(argc, argv);
 
@@ -22,6 +28,9 @@ int main(int argc, char **argv){
       break;
     CASE ("_mapAttrShouldBeNotNull")
       it("Atributo _map de uma nova Lista não pode ser NULL", _mapAttrShouldBeNotNull);
+      break;
+    CASE ("destroyShouldBeNotNull")
+      it("Atributo destroy tem valor válido (não nulo)", destroyShouldBeNotNull);
       break;
     DEFAULT
       noTestFoundWithGiven(args.testName);
