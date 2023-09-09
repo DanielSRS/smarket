ba339c0bcd5fa4c86b3f9057fa54c499be95b5d3
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2023-09-08 22:33:14 -0300

    ✨ Adiciona cliente TCP

--- src/Apps/SensorManager/SensorManager.c
+++ src/Apps/SensorManager/SensorManager.c
@@ -2,16 +2,32 @@
 #include "../../util/ReadTags/ReadTags.h" //readtags
 #include <unistd.h> // close
 #include <stdio.h> // printf
+#include <string.h> // strlen
+
+void sendRequest(TCPClient* tcpClient);
 
 int main() {
-    int sockfd = connectTo("127.0.0.1", 3492);
+    TCPClient* tcpClient = newTCPClient("127.0.0.1", 3490);
     Map *tagsList = readTags();
 
     printf("\n\nTags lidas: \n%s", tagsList->toString(tagsList));
  
     // function for chat
-    sendRequest(sockfd);
- 
-    // close the socket
-    close(sockfd);
+    sendRequest(tcpClient);
+    tcpClient->close(tcpClient);
+}
+
+void sendRequest(TCPClient* tcpClient) {
+    char *request = "GET /dan/niel?dfg=dsfg HTTP/1.1\
+                    \r\nHost: localhost:3492\
+                    \r\nContent-Type: application/json\
+                    \r\nUser-Agent: SensorManager/0.0.1\
+                    \r\nContent-Length: 17\
+                    \r\n\r\n{\"with\": \"tcpclient\"}";
+
+    int numberOfBytesSent = tcpClient->send(tcpClient, strlen(request), request);
+    if (numberOfBytesSent == -1)
+        perror("send");
+
+    printf("\nRequest sent\n\n");
 }
--- src/util/Socket/socket.h
+++ src/util/Socket/socket.h
@@ -8,7 +8,6 @@ struct addrinfo* getAddressInfo(void (*onError)());
 int createAndBindSocket(uint16_t port, void (*onError)());
 void handleConnectionOnANewProcess(TCPConnection* newConnection);
 void listenForConnections(uint16_t port, int socketFileDescriptor, void (*handdler)(TCPConnection* newConnection), void (*onError)());
-void sendRequest(int sockfd);
 int connectTo(char *host, int port);
 
 #endif
